c8523810507c07254223fb91262b3d3fd4a6ba74
Alex Tuft <alex.tuft@outlook.com>
2023-08-13 18:37:02 +0100

    Modify page up and down motions

--- init.lua
+++ init.lua
@@ -27,6 +27,8 @@ require("lazy").setup("plugins")
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file [e]xplorer" })
 
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 --------------------------------------------------------------------------------
 --- Options
