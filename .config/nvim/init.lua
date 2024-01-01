6aaf3d386f52bacd3256e1b0741d250ee80ab5c1
Alex Tuft <alex.tuft@outlook.com>
2024-01-01 17:41:16 +0000

    Change harpoon keys

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -88,14 +88,17 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>ha", function () harpoon:list():append() end, { desc = "Add file to harpoon" })
-vim.keymap.set("n", "<leader>hl", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "List harpooned files"})
+vim.keymap.set("n", "<leader>ha", function () harpoon:list():append() end, { desc = "[A]dd file to [h]arpoon" })
+vim.keymap.set("n", "<leader>hr", function () harpoon:list():remove() end, { desc = "[R]emove file to [h]arpoon" })
+vim.keymap.set("n", "<leader>hc", function () harpoon:list():clear() end, { desc = "[C]clear [h]arpooned files" })
+vim.keymap.set("n", "<leader>hl", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "[L]ist [h]arpooned files"})
 
-vim.keymap.set("n", "<leader>hy", function () harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>hu", function () harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>hi", function () harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>ho", function () harpoon:list():select(4) end)
-vim.keymap.set("n", "<leader>hp", function () harpoon:list():select(5) end)
+for i=1,9 do
+    vim.keymap.set("n", "<leader>h" .. i, function () harpoon:list():select(i) end, { desc = "Select [h]arpooned file [" .. i .. "]" })
+end
+
+vim.keymap.set("n", "<leader>hn", function () harpoon:list():next() end, { desc = "[N]ext [h]arpooned file" })
+vim.keymap.set("n", "<leader>hp", function () harpoon:list():prev() end, { desc = "[P]revious [h]arpooned file" })
 
 -- WhichKey
 
