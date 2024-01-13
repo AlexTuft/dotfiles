a7bc3195188a318290cdc3d2906fc276b140455c
Alex Tuft <alex.tuft@outlook.com>
2024-01-13 12:47:26 +0000

    Don't open project if user quits open project

--- bin/open-project.sh
+++ bin/open-project.sh
@@ -4,6 +4,10 @@ work_dir=~/sources/
 
 workspace_dir=$(find $work_dir -maxdepth 1 -type d | fzf)
 
+if [ -z "$workspace_dir" ]; then
+    exit 0
+fi
+
 session_name=$(basename $workspace_dir)
 tmux new-session -A -s $session_name -c $workspace_dir
 
