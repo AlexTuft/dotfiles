db7f7f1883f5b29898712b7912ad5071e1ad6155
Alex Tuft <alex.tuft@outlook.com>
2024-07-14 20:43:42 +0100

    i won't write another rust script template

--- .config/shell/bindings.sh
+++ .config/shell/bindings.sh
@@ -1,3 +1,4 @@
 bind -x '"\C-f":~/bin/open-project.sh'
 bind -x '"\C-o":~/bin/attach-tmux-session.sh'
 bind -x '"\C-a":tmux a'
+bind -x '"\C-n":new'
--- .config/shell/bindings_zsh.sh
+++ .config/shell/bindings_zsh.sh
@@ -1,3 +1,4 @@
 bindkey -s "\C-f" "~/bin/open-project.sh\n"
 bindkey -s "\C-o" "~/bin/attach-tmux-session.sh\n"
 bindkey -s "\C-a" "tmux a\n"
+bindkey -s "\C-n" "new\n"
